9c45610829749c51f876cfa658392641a7cb760a
iffat <iffat@gmail.com>
2025-11-28 23:28:28 +0500

    data list

---
+++ file.py
@@ -0,0 +1,11 @@
+data = {
+    "name": "Iffat",
+    "age": 22
+}
+def get_name():
+    return data["name"]
+def get_age():
+    return data["age"]
+print(get_name())
+print(get_age())
+print("Hello, World!")
